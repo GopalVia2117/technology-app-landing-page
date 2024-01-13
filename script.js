ba541637e224d6e60bd6366356a33b5034b8d68b
GopalVia2117 <gopals2117@gmail.com>
2024-01-13 20:04:23 +0530

    Updates

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 $('.brand-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+$('.screenshots-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    // nav:true,
+    dots: true,
     autoplay: true,
     responsive:{
         0:{
@@ -11,7 +31,7 @@ $('.brand-carousel').owlCarousel({
             items:3
         },
         1000:{
-            items:5
+            items:4
         }
     }
 })
